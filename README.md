c9c71d6e9967fa333df28ae0228c3c4988a94a18
namYeJi9q <nyj5850@naver.com>
2024-03-13 01:56:23 +0900

    [feat]layout 틀 잡기, 경로 폴더 만들기

--- README.md
+++
@@ -1,2 +0,0 @@
-# portfolio
-Frontend developer 남예지 Portfolio
---
+++ yeji-portfolio/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+    return (
+        <>
+            <h1>404페이지 입니다.</h1>
+            <p>홈으로</p>
+        </>
+    );
+}
